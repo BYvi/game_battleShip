21cf7cc44eebc73cd8fda2652cf2c61beaf24add
BYvi <breuy@mediamarktsaturn.com>
2019-09-04 10:57:14 +0200

    added classes

---
+++ sinkShips.js
@@ -0,0 +1,41 @@
+let battleShips = new battleShips (ships);
+
+let ship1 = []
+
+class battleShip {
+    constructor (ships){
+        this.hits = 0,
+        this.guesses = 0,
+        this.sunk = false,
+        this.shipPosition = [],
+        this.water = [],
+        his.round = 0
+    }
+}
+
+class Game {
+    constructor() {
+    this.player = [];
+    this.coordinatemap = [[],[]];
+    }
+
+    start() {
+
+    }
+
+    shoot() {
+
+    }
+}
+
+
+
+
+
+//definition of matchfield - coveredFields - choosed in function of ship
+
+// Positions of ships
+
+// Shuffle function for new positions of ships
+// class defintion battleships
+
