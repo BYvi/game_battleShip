914cf2a14fc1bed7176352a09d31f89565bd0edf
BYvi <breuy@mediamarktsaturn.com>
2019-09-09 17:52:13 +0200

    added and guess to the shoot function

--- sinkShips.js
+++ sinkShips.js
@@ -1,25 +1,24 @@
 class BattleShip {
-    constructor(shipHeight, direction) {
+    constructor(shipHeight, direction, backgroundColor) {
         this.hits = [];
-        this.sunk = false;
-        this.direction = direction; 
+        this.sunk = [];
+        this.direction = direction;
         this.shipHeight = shipHeight;
         this.shipPosition = [];
+        this.color = backgroundColor;
     }
 
-    
-
     setShipLocation() {
-        const rowLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+        const rowLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
         if (this.direction == "horizontal") {
-            const row = rowLetters[Math.floor(Math.random() * (10 - this.shipHeight + 1))];
-            const startColumn = Math.floor(Math.random() * (10 + 1));
+            const row = rowLetters[Math.floor(Math.random() * 10)];
+            const startColumn = Math.floor(Math.random() * (10 - this.shipHeight));
             for (let column = startColumn; column < startColumn + this.shipHeight; column++) {
                 this.shipPosition.push(`${row}${column}`);
             }
         } else { //vertical
-            const startRow = Math.floor(Math.random() * (10 + 1));
-            const column = Math.floor(Math.random() * (10 - this.shipHeight + 1));
+            const startRow = Math.floor(Math.random() * (10 - this.shipHeight + 1));
+            const column = Math.floor(Math.random() * (10 + 1));
             for (let index = startRow; index < startRow + this.shipHeight; index++) {
                 let row = rowLetters[index]
                 this.shipPosition.push(`${row}${column}`);
@@ -27,18 +26,16 @@ class BattleShip {
 
         }
     }
-    checkCollision(otherShip){
-        for (let index = 0; index < this.shipPosition.length; index++){
-            if (this.shipPosition.includes(otherShip.shipPosition [index]))
-              return true;            
+    checkCollision(otherShip) {
+        for (let index = 0; index < this.shipPosition.length; index++) {
+            if (this.shipPosition.includes(otherShip.shipPosition[index]))
+                return true;
             else {
-              return false;
-     }
+                return false;
+            }
 
-     }
-
-
-} 
+        }
+    }
 }
 
 class Game {
@@ -48,6 +45,7 @@ class Game {
         this.guesses = 0;
         this.round = 0;
         this.battleShips = [];
+        this.shipsSunk = 0;
 
     }
 
@@ -56,38 +54,37 @@ class Game {
         this.battleShips.push(ship1);
         ship2.setShipLocation();
         let luckyCounter = 0;
-        while(ship1.checkCollision(ship2) && luckyCounter < 100){
+        while (ship1.checkCollision(ship2) && luckyCounter < 100) {
             luckyCounter++;
         }
         this.battleShips.push(ship2);
-        
-        
-
     }
+
+
+
 
     shoot(event) {
         const shotSquareId = event.target.id;
 
-        this.battleShips.forEach( currentShip => {
+        this.battleShips.forEach(currentShip => {
 
-        if (currentShip.shipPosition.includes(shotSquareId)) {
-            currentShip.hits.push(`${shotSquareId}`);
-            event.target.innerText = "Hit!";
-            const message = document.getElementById(shotSquareId).style.backgroundColor = "red";
-        
-        } else {
-            event.target.innerText = "Water!"
-        }
-    })
+            if (currentShip.shipPosition.includes(shotSquareId)) {
+                currentShip.hits.push(`${shotSquareId}`);
+                event.target.innerText = "Hit!";
+            } else {
+                event.target.innerText = "Water!"
+            }
+        })
+                this.guesses++;
+                const counting = document.getElementById("guess").innerText = (`Guesses: ${this.guesses}`);
     }
 }
 
 
-let ship1 = new BattleShip(4, "horizontal");
-let ship2 = new BattleShip(5, "vertical");
+let ship1 = new BattleShip(4, "horizontal", "red");
+let ship2 = new BattleShip(5, "vertical", "blue");
 
 let game = new Game();
 game.start(ship1, ship2);
-//Array.from(document.getElementsByClassName("cell")).forEach(cell => cell.onclick = game.shoot);
 
 document.getElementById("sea").onclick = () => game.shoot(event);
